668b03136249a6b2c868eb6bc24cd679937408bc
vamshikumar7735300462 <154436703+vamshikumar7735300462@users.noreply.github.com>
2025-07-10 14:59:58 +0530

    first commit

--- nlak.py
+++ nlak.py
@@ -17,5 +17,21 @@
 #         lst.append(i)
 # print(lst)
 # print(set(tok)-set(lst))
-import nltk
-nltk.download("wordnet")
+# import nltk
+# nltk.download("wordnet")
+from nltk.tokenize import word_tokenize as wt
+from nltk.tokenize import sent_tokenize as st
+from nltk.corpus import stopwords as sw
+text=" Any person who comes in the store today is eligible for the discount. offert at the eand"
+tok=wt(text)
+sot=st(text)
+print("work tokenize",tok)
+print("sent tokenize",sot)
+s=sw.words("english")
+print("stop words",s)
+lst=[]
+for i in tok:
+    if i not in s:
+        lst.append(i)
+print("after removing stop words",lst)
+print("stops words are deleted ",set(tok)-set(lst))
