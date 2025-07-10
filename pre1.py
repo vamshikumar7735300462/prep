5feb180cf9818f062fa5e3ef09950ed9aa591a48
vamshikumar7735300462 <154436703+vamshikumar7735300462@users.noreply.github.com>
2025-07-10 14:10:06 +0530

    first commit

---
+++ pre1.py
@@ -0,0 +1,16 @@
+from nltk.tokenize import word_tokenize as wt
+from nltk.tokenize import sent_tokenize as st
+from nltk.corpus import stopwords as sw
+text=" Any person who comes in the store today is eligible for the discount. offert at the eand"
+tok=wt(text)
+sot=st(text)
+print("work tokenize",tok)
+print("sent tokenize",sot)
+s=sw.words("english")
+print("stop words",s)
+lst=[]
+for i in tok:
+    if i not in s:
+        lst.append(i)
+print("after removing stop words",lst)
+print("stops words are ",set(tok)-set(lst))
