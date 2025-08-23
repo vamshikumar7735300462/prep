ea004d12ab61e3cb740871d137db8dc541590094
vamshikumar7735300462 <154436703+vamshikumar7735300462@users.noreply.github.com>
2025-08-23 11:10:26 +0530

    Delete ptk.py

--- ptk.py
+++
@@ -1,6 +0,0 @@
-import nltk
-nltk.download()
-nltk.download('punkt')        # For tokenization
-nltk.download('stopwords')    # For common stopwords
-nltk.download('wordnet')      # For lemmatization
-nltk.download('averaged_perceptron_tagger')
